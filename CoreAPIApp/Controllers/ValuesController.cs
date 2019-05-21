ca8eb8115543095454febd17b2a6894ba57032a8
Saurabh Vartak <savartak@microsoft.com>
2019-05-21 20:30:23 +0530

    modified the controller

--- CoreAPIApp/Controllers/ValuesController.cs
+++ CoreAPIApp/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace CoreAPIApp.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value23" };
         }
 
         // GET api/values/5
